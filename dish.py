65df1b3b91d4d82197ef30f1c0db2ca537e70b8f
Audrius_spiridavicius <audriusspiridavicius@gmail.com>
2024-09-04 10:58:42 +0300

    dish class

---
+++ dish.py
@@ -0,0 +1,24 @@
+from .interfaces.dish import IDish
+
+
+class Dish(IDish):
+    
+    def __init__(self) -> None:
+        super().__init__()
+    
+    def get_price(self) -> float:
+        return self.price
+
+    def get_rating(self):
+        
+        number_of_records = len(self.ratings)
+        
+        average_ranting = 0
+        
+        if number_of_records > 0:
+            average_ranting = sum([rating.rating for rating in self.ratings]) / number_of_records
+            average_ranting = round(average_ranting, 2)
+                
+        return average_ranting
+
+    
