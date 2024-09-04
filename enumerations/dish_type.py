a2d402e33b11f10450686e8e3c28cc16df0ba564
Audrius_spiridavicius <audriusspiridavicius@gmail.com>
2024-09-04 10:58:05 +0300

    dish type enum created

---
+++ enumerations/dish_type.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+class DishType(Enum):
+    """Enum for dish types"""
+    NONE = 0
+    APPETIZER = 1
+    SOUP = 2
+    SALAD = 3
+    PASTA = 4
+    SEEFOOD = 5
+    MEAT = 6
